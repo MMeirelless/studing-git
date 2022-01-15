15af086ac2aad6040dc300624a91926d545b2db1
Matheus Meirelles <matheus.meirellessilva@hotmail.com>
2022-01-15 17:45:18 -0300

    Criando calculadora básica para ser utilizada no estudo do Git

---
+++ calc.py
@@ -0,0 +1,38 @@
+class Calc():
+
+    # Cosntructor
+    def __init__(self, n1=0, n2=0):
+        self.n1 = n1
+        self.n2 = n2
+
+        pass
+
+    # Operations
+    def sum(self):
+        return self.n1 + self.n2
+
+    def sub(self):
+        return self.n1 - self.n2
+    
+    def mult(self):
+        return self.n1 * self.n2
+    
+    def div(self):
+        return self.n1 / self.n2        
+
+    # Calculate
+    def calculate(self, op):
+        if op == 1:
+            return self.sum()
+
+        elif op == 2:
+            return self.sub()
+
+        elif op == 3:
+            return self.div()
+
+        elif op == 4:
+            return self.mult()
+
+        else:
+            return "Invalid operation option"
